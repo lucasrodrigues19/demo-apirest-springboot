4365fd88c448fe972349d91658191ae2015aff03
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-09 21:36:32 -0300

    Associacao de muitos para muitos com Jointable

--- src/main/java/com/lrvendas/springbootex/config/TestConfig.java
+++ src/main/java/com/lrvendas/springbootex/config/TestConfig.java
@@ -40,14 +40,6 @@ public class TestConfig implements CommandLineRunner {
 //dataseeding
 	@Override
 	public void run(String... args) throws Exception {// vai ser executado quando a aplicacao for iniciadas
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		userRepository.saveAll(Arrays.asList(u1, u2));
-
-		Order o1 = new Order(null, OrderStatus.PAID, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, OrderStatus.WAITTING_PAYAMENT,Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null,OrderStatus.CANCELED, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
@@ -60,5 +52,21 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+	
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		userRepository.saveAll(Arrays.asList(u1, u2));
+
+		Order o1 = new Order(null, OrderStatus.PAID, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, OrderStatus.WAITTING_PAYAMENT,Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null,OrderStatus.CANCELED, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
--- src/main/java/com/lrvendas/springbootex/entites/Product.java
+++ src/main/java/com/lrvendas/springbootex/entites/Product.java
@@ -8,15 +8,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,13 +27,14 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
-	@Transient //para que o jpa não reconheca
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "fk_product_id"), //
+	inverseJoinColumns = @JoinColumn(name = "fk_category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
-			
-	
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
@@ -39,8 +43,6 @@ public class Product implements Serializable{
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
-
-
 
 	public Long getId() {
 		return id;
@@ -70,31 +72,21 @@ public class Product implements Serializable{
 		return price;
 	}
 
-
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
-
 
 	public String getImgUrl() {
 		return imgUrl;
 	}
 
-
-
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
 
-
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
-
 
 	@Override
 	public int hashCode() {
@@ -125,6 +117,5 @@ public class Product implements Serializable{
 	public String toString() {
 		return "Product [id=" + id + ", name=" + name + ", description=" + description + "]";
 	}
-	
-	
+
 }
