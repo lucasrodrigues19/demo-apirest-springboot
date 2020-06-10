3c6c3651bc2240e350bcc27777906e3013b87a79
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-10 03:38:40 -0300

    Tratamento de exceção delete

--- src/main/java/com/lrvendas/springbootex/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lrvendas/springbootex/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lrvendas.springbootex.services.exceptions.DatabaseException;
 import com.lrvendas.springbootex.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice//interceptar as excessoes que ocorrerem para que esse objeto possa dar um possivel tratamento
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.ok().body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class) //interceptar a requisição que gerou a exessao
+	public ResponseEntity<StandardError> resourceNotFound(DatabaseException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.ok().body(err);
 	}
--- src/main/java/com/lrvendas/springbootex/services/UserService.java
+++ src/main/java/com/lrvendas/springbootex/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.lrvendas.springbootex.entites.User;
 import com.lrvendas.springbootex.repositories.UserRepository;
+import com.lrvendas.springbootex.services.exceptions.DatabaseException;
 import com.lrvendas.springbootex.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,8 +33,15 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
-		
+		}catch(EmptyResultDataAccessException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			e.printStackTrace();
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id); //vai me da um objeto monitorado
